60d5b24bc71ca1611dbbc26e8f403c2bc688941d
kimblerbr <carlosprado.br@gmail.com>
2017-01-12 14:35:32 -0300

    1

---
+++ bioid.go
@@ -0,0 +1,144 @@
+package bioid
+
+import (
+	"errors"
+	"fmt"
+	"regexp"
+	"strings"
+
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+)
+
+type SimpleChaincode struct {
+}
+
+func main() {
+	err := shim.Start(new(SimpleChaincode))
+	if err != nil {
+		fmt.Printf("Error starting Simple chaincode: %s", err)
+	}
+}
+
+// Init resets all the things
+func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+	if len(args) != 1 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 1")
+	}
+
+	err := stub.PutState("hello_world", []byte(args[0]))
+	if err != nil {
+		return nil, err
+	}
+
+	return nil, nil
+}
+
+// Invoke isur entry point to invoke a chaincode function
+func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+	fmt.Println("invoke is running " + function)
+
+	a,b,c,d,e string
+	
+	a = args[0]
+	b = args[1]
+	c = args[2]
+	d = args[3]
+	
+	e := removeCPFMask(a)
+	kv := [2]string{e,b}	
+
+	// Handle different functions
+	if function == "init" {
+		return t.Init(stub, "init", args)
+	} else if function == "write" {
+		return t.write(stub, kv)
+	} else if function == "read" {
+		return t.read(stub, args)
+	}
+	fmt.Println("invoke did not find func: " + function)
+
+	return nil, errors.New("Received unknown function invocation: " + function)
+	
+
+	
+	
+}
+
+// Query is our entry point for queries
+func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+	fmt.Println("query is running " + function)
+
+	// Handle different functions
+	if function == "read" { //read a variable
+		return t.read(stub, args)
+	}
+	fmt.Println("query did not find func: " + function)
+
+	return nil, errors.New("Received unknown function query: " + function)
+}
+
+// write - invoke function to write key/value pair
+func (t *SimpleChaincode) write(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	var key, value string
+	var err error
+	fmt.Println("running write()")
+
+	if len(args) != 2 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 2. name of the key and value to set")
+	}
+
+	key = args[0] //rename for funsies
+	value = args[1]
+	err = stub.PutState(key, []byte(value)) //write the variable into the chaincode state
+	if err != nil {
+		return nil, err
+	}
+	return nil, nil
+}
+
+// read - query function to read key/value pair
+func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	var key, jsonResp string
+	var err error
+
+	if len(args) != 1 {
+		return nil, errors.New("Incorrect number of arguments. Expecting name of the key to query")
+	}
+
+	key = args[0]
+	valAsbytes, err := stub.GetState(key)
+	if err != nil {
+		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
+		return nil, errors.New(jsonResp)
+	}
+
+	return valAsbytes, nil
+}
+
+func removeCPFMask(CPF string) (string, error) {
+	if len(CPF) < 11 {
+		return nil, errors.New("Incorrect length of parameter: CPF. Expecting 11 characters or more.")
+	}
+	var a, b string
+	a = strings.Replace(CPF, ".", "", -1)
+	b = strings.Replace(a, "-", "", -1)
+	var re := regexp.MustCompile("[0-9]")
+	
+	if re.MatchString(b) {
+	   return b
+	} else {
+	  	return nil, errors.New("Incorrect value of parameter: CPF. Expecting 11 numbers.")
+	}
+}
+
+func isCPF(CPF string) (string, error) {
+     var a, b string
+	 a = removeCPFMask(CPF)
+	 s := len(a)
+	
+	  for _, r := range a {
+        c := string(r)
+        fmt.Println(c)
+    }
+	
+}
